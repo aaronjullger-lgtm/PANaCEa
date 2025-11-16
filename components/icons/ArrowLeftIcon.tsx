3398b7dedba0f19e8f79b59ea9a8e440a3e30f3c
aaronjullger-lgtm <aaronjullger@gmail.com>
2025-11-16 16:31:24 -0500

    Create ArrowLeftIcon.tsx

---
+++ components/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,17 @@
+// icons/ArrowLeftIcon.tsx
+import React from "react";
+
+export const ArrowLeftIcon = ({ className = "" }) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    className={className}
+  >
+    <path 
+      fillRule="evenodd" 
+      d="M10.53 3.47a.75.75 0 010 1.06L5.81 9.25H21a.75.75 0 010 1.5H5.81l4.72 4.72a.75.75 0 11-1.06 1.06l-6-6a.75.75 0 010-1.06l6-6a.75.75 0 011.06 0z" 
+      clipRule="evenodd"
+    />
+  </svg>
+);
