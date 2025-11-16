53a84da60e3a18bf6e9ddc2cb0afba26037922d7
aaronjullger-lgtm <aaronjullger@gmail.com>
2025-11-16 16:31:11 -0500

    Create ClearHighlightIcon.tsx

---
+++ components/icons/ClearHighlightIcon.tsx
@@ -0,0 +1,13 @@
+// icons/ClearHighlightIcon.tsx
+import React from "react";
+
+export const ClearHighlightIcon = ({ className = "" }) => (
+  <svg 
+    xmlns="http://www.w3.org/2000/svg" 
+    viewBox="0 0 24 24" 
+    fill="currentColor"
+    className={className}
+  >
+    <path d="M3 17.25l8.49-8.49 5.3 5.3L8.29 22H3v-4.75zM20.71 7.04a1 1 0 000-1.41L18.37 3.3a1 1 0 00-1.41 0l-2.88 2.88 3.75 3.75 2.88-2.89z" />
+  </svg>
+);
