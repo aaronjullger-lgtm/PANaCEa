b3fd964a57e52e6a52a29c31426e7bd0941c5ca8
aaron <aaronjullger@gmail.com>
2025-12-19 13:19:21 -0500

    router issue and instructions for copilot added

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './components/AuthProvider';
@@ -16,7 +17,9 @@ root.render(
   <React.StrictMode>
     <ErrorBoundary>
       <AuthProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </AuthProvider>
     </ErrorBoundary>
   </React.StrictMode>
